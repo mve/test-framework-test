454b3d8336c84979474fac0ce187987fb76c3ba7
Ruben <RAW.Kroese@student.han.nl>
2021-04-19 14:19:23 +0200

    Jest added test

--- Node/src/index.js
+++ Node/src/index.js
@@ -8,3 +8,10 @@ app.get('/', function (req, res) {
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
 });
+
+
+function sum(a, b) {
+    return a + b;
+  }
+   
+  export default sum;
